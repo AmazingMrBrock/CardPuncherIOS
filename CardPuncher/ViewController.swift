1c90f6ab3f237c718f5b2f53e2d8ff98fb96d911
AmazingMrBrock <dramascus@gmail.com>
2016-12-04 11:34:26 -0800

    Initial Commit

---
+++ CardPuncher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardPuncher
+//
+//  Created by Brock Swann on 2016-12-04.
+//  Copyright © 2016 Brock Swann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CardPuncherTests/CardPuncherTests.swift
@@ -0,0 +1,36 @@
+//
+//  CardPuncherTests.swift
+//  CardPuncherTests
+//
+//  Created by Brock Swann on 2016-12-04.
+//  Copyright © 2016 Brock Swann. All rights reserved.
+//
+
+import XCTest
+@testable import CardPuncher
+
+class CardPuncherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
